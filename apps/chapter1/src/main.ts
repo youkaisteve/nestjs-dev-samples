9124287323bb72a744c8920b873cf5f253ecd84a
youkaisteve <youkaisteve@gmail.com>
2021-12-25 21:38:21 +0800

    refactor: 💡 fix

--- apps/chapter1/src/main.ts
+++ apps/chapter1/src/main.ts
@@ -4,5 +4,8 @@ import { Chapter1Module } from './chapter1.module';
 async function bootstrap() {
   const app = await NestFactory.create(Chapter1Module);
   await app.listen(3000);
+  setTimeout(() => {
+    app.close();
+  }, 5000)
 }
 bootstrap();
--- apps/chapter1/src/modules/user/user.service.ts
+++ apps/chapter1/src/modules/user/user.service.ts
@@ -3,15 +3,35 @@ import { UserEntity } from '../../entities/user.entity';
 import { CreateUserDto } from './dtos/create.user.dto';
 import { CreateUserRespDto } from './dtos/create.user.resp.dto';
 import * as _ from 'lodash';
-import { Injectable } from '@nestjs/common';
+import { BeforeApplicationShutdown, Injectable, OnApplicationBootstrap, OnApplicationShutdown, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {
+export class UserService implements OnModuleInit, OnApplicationBootstrap, OnModuleDestroy, BeforeApplicationShutdown, OnApplicationShutdown {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepo: Repository<UserEntity>,
-  ) {}
+  ) { }
+
+  onApplicationShutdown(signal?: string) {
+    console.log(`onApplicationShutdown -> ${UserService.name}`)
+  }
+
+  beforeApplicationShutdown(signal?: string) {
+    console.log(`beforeApplicationShutdown -> ${UserService.name}`)
+  }
+
+  onModuleDestroy() {
+    console.log(`onModuleDestroy -> ${UserService.name}`)
+  }
+
+  onApplicationBootstrap() {
+    console.log(`onApplicationBootstrap -> ${UserService.name}`)
+  }
+
+  onModuleInit() {
+    console.log(`onMuduleInit -> ${UserService.name}`)
+  }
 
   async create(createUserDto: CreateUserDto): Promise<CreateUserRespDto> {
     const user = await this.userRepo.save(createUserDto);
